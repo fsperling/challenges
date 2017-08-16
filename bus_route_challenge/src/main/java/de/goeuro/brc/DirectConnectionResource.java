7061db97e73d16904d58d449b8da4ad9fac537eb
Felix Sperling <f.sperling@gmx.de>
2017-08-16 16:49:11 +0700

    add error messages when parameters arent correct

--- bus_route_challenge/src/main/java/de/goeuro/brc/DirectConnectionResource.java
+++ bus_route_challenge/src/main/java/de/goeuro/brc/DirectConnectionResource.java
@@ -3,7 +3,6 @@ package de.goeuro.brc;
 import javax.json.*;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -12,35 +11,29 @@ import javax.ws.rs.core.Response;
 public class DirectConnectionResource {
 
     @GET
-    @Produces({ MediaType.APPLICATION_JSON })
     public Response direct(
-            @QueryParam("dep_sid") Integer dep_sid, 
+            @QueryParam("dep_sid") Integer dep_sid,
             @QueryParam("arr_sid") Integer arr_sid) {
-        
-      System.out.println("dep_sid: " + dep_sid);
-      System.out.println("arr_sid: " + arr_sid);
-      
-      if (dep_sid == null || arr_sid == null) {
-          return Response.status(Response.Status.BAD_REQUEST).build();
-      }
-      
-      if (dep_sid.equals(arr_sid)) {
-             System.out.println("start and stop are the same");
-             return Response.status(Response.Status.BAD_REQUEST).build();
 
-      }
-      
-      boolean directConnection = DirectBusConnectionService.getBusNetwork().hasDirectConnection(dep_sid, arr_sid);
-      
-      JsonObject value = Json.createObjectBuilder()
-        .add("dep_sid", dep_sid)
-        .add("arr_sid", arr_sid)
-        .add("direct_bus_route", directConnection)
-        .build();
+        if (dep_sid == null || arr_sid == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("dep_sid or arr_sid is missing").build();
+        }
 
-      return Response.ok(value.toString(), MediaType.APPLICATION_JSON).build();      
+        if (dep_sid.equals(arr_sid)) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("dep_sid and arr_sid can not be the same").build();
+        }
+
+        boolean directConnection = DirectBusConnectionService.getBusNetwork().hasDirectConnection(dep_sid, arr_sid);
+
+        JsonObject value = Json.createObjectBuilder()
+                .add("dep_sid", dep_sid)
+                .add("arr_sid", arr_sid)
+                .add("direct_bus_route", directConnection)
+                .build();
+
+        return Response.ok(value.toString(), MediaType.APPLICATION_JSON).build();
     }
-                
-            
-            
+
 }
